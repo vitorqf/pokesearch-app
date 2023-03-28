2a7da5b1372f4cb288962b1d8f890759dca682cf
Vitor Rafael <rqfvitor@gmail.com>
2023-03-28 20:14:37 -0300

    chore: add badge colors

---
+++ App.tsx
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import { ThemeProvider } from 'styled-components'
+
+import { theme } from './styles/theme'
+
+export default function App(): React.ReactNode {
+  return (
+    <ThemeProvider theme={theme}>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style='auto' />
+      </View>
+    </ThemeProvider>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
---
+++ styles/theme.ts
@@ -0,0 +1,28 @@
+export const theme = {
+  colors: {
+    background: '#f5f5f5',
+    gray100: '#F1F5F9',
+    badge: {
+      colors: {
+        water: '#6890F0',
+        dragon: '#7038F8',
+        dark: '#705848',
+        ghost: '#705898',
+        grass: '#78C850',
+        ice: '#98D8D8',
+        poison: '#A040A0',
+        flying: '#A890F0',
+        normal: '#A8A878',
+        bug: '#A8B820',
+        rock: '#B8A038',
+        steel: '#B8B8D0',
+        fighting: '#C03028',
+        ground: '#E0C068',
+        fairy: '#EE99AC',
+        fire: '#F05030',
+        psychic: '#F85888',
+        electric: '#F8D030',
+      },
+    },
+  },
+}
